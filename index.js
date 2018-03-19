5123682ba5a6dd21921279443a684e76b2839ad3
Edward M <edward.m1017@gmail.com>
2018-03-19 22:27:06 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
-var name = "susan"
+var name = "Susan"
 var height = "70"
 
-if(name === "susan") { console.log("The name is correct") }
+if(name === "Susan") { console.log("The name is correct") }
 
 if(height === 70) {console.log("the height is correct") }
 
